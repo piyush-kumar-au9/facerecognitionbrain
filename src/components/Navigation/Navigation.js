a6c900717a2b8ff380a9c6c38f8f43b488a3e6bd
piyush-kumar-au9 <piyush.16bcon546@jecrcu.edu.in>
2021-06-01 02:23:51 +0530

    Made changes in the route options

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,7 +4,7 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
 	if(isSignedIn){		
 	return(
 		<nav style = { { display : "flex",  justifyContent: "flex-end"} }>
-			<span onClick={() => onRouteChange('signin')}className="f3 link dim black underline pa3 pointer">Sign Out</span>
+			<span onClick={() => onRouteChange('signout')}className="f3 link dim black underline pa3 pointer">Sign Out</span>
 		</nav>
 		);
 	}
